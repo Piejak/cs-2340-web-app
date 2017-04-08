f63ba885965857583871af141f1c983be804f8ca
Brian Piejak <Brian.Piejak@gmail.com>
2017-04-07 21:17:28 -0400

    rework some styling with index

--- WaterLogged/app/controllers/reports_controller.rb
+++ WaterLogged/app/controllers/reports_controller.rb
@@ -1,5 +1,7 @@
 class ReportsController < ApplicationController
 
+    before_action :set_report, only: [:show, :edit, :update, :destroy]
+
     def index
         @reports = Report.all
     end
@@ -9,7 +11,33 @@ class ReportsController < ApplicationController
     end
 
     def create
-        @report = Report.create(report_params)
+        if @report = Report.create(report_params)
+            flash[:success] = "Report was created successfully"
+            redirect_to reports_path
+        else
+            flash.now[:alert] = "Report couldn't be created"
+            render :new
+        end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        if @report.update(report_params)
+            flash[:success] = "Report was updated"
+            redirect_to(report_path(@report))
+        else
+            flash.now[:alert] = "Update failed"
+            render :edit
+        end
+    end
+
+    def destroy
+        @report.destroy
         redirect_to reports_path
     end
 
@@ -17,5 +45,9 @@ class ReportsController < ApplicationController
 
     def report_params
         params.require(:report).permit(:condition, :watertype, :latitude, :longitude)
+    end
+
+    def set_report
+        @report = Report.find(params[:id])
     end
 end
